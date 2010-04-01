154ddfb6960a461bfac864ba91f4d25e6f354455
Dan Mohl <dmohl@yahoo.com>
2010-04-01 16:52:10 -0500

    Added functionality to check both nodes for the value

--- Polyphony/ChordClient.fs
+++ Polyphony/ChordClient.fs
@@ -17,13 +17,18 @@ let Initialize() =
 let RunCommand(input:string) : unit =
     
     let inputArguments = input.Split(' ')
-    let result = match inputArguments.[0] with
-                 | "put" -> 
-                     let localObject = Activator.GetObject(typeof<Shared.Chord>, selfServerInformation) :?> Shared.Chord     
-                     localObject.PutValueByKey inputArguments.[1] inputArguments.[2] |> ignore
-                     sprintf "PUT Key:%A Value:%A" inputArguments.[1] inputArguments.[2] :> obj   
-                 | "get" -> 
-                     let remoteObject = Activator.GetObject(typeof<Shared.Chord>, serverInformation) :?> Shared.Chord     
-                     remoteObject.GetValueByKey inputArguments.[1]
-                 | _ -> "unknown command" :> obj   
+    let result = 
+        match inputArguments.[0] with
+        | "put" -> 
+            let localObject = Activator.GetObject(typeof<Shared.Chord>, selfServerInformation) :?> Shared.Chord     
+            localObject.PutValueByKey inputArguments.[1] inputArguments.[2] |> ignore
+            sprintf "PUT Key:%A Value:%A" inputArguments.[1] inputArguments.[2] :> obj   
+        | "get" -> 
+            let rec getValue server =
+                let chord = Activator.GetObject(typeof<Shared.Chord>, server) :?> Shared.Chord         
+                match chord.GetValueByKey inputArguments.[1] with
+                | null -> getValue serverInformation    
+                | value -> value
+            getValue selfServerInformation
+        | _ -> "unknown command" :> obj   
     Console.WriteLine(result) |> ignore
