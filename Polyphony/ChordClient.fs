abf1483f2c8e99fb4314b8bd341dcd5e20116650
Dan Mohl <dmohl@yahoo.com>
2010-04-01 15:23:05 -0500

    Added put and get commands

--- Polyphony/ChordClient.fs
+++ Polyphony/ChordClient.fs
@@ -15,6 +15,15 @@ let Initialize() =
     localObject.PutValueByKey "test" "testValue"
 
 let RunCommand(input:string) : unit =
-    let remoteObject = Activator.GetObject(typeof<Shared.Chord>, serverInformation) :?> Shared.Chord 
-    let result = remoteObject.GetValueByKey input
+    
+    let inputArguments = input.Split(' ')
+    let result = match inputArguments.[0] with
+                 | "put" -> 
+                     let localObject = Activator.GetObject(typeof<Shared.Chord>, selfServerInformation) :?> Shared.Chord     
+                     localObject.PutValueByKey inputArguments.[1] inputArguments.[2] |> ignore
+                     sprintf "PUT Key:%A Value:%A" inputArguments.[1] inputArguments.[2] :> obj   
+                 | "get" -> 
+                     let remoteObject = Activator.GetObject(typeof<Shared.Chord>, serverInformation) :?> Shared.Chord     
+                     remoteObject.GetValueByKey inputArguments.[1]
+                 | _ -> "unknown command" :> obj   
     Console.WriteLine(result) |> ignore
