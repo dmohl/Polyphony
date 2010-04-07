a4f5816a21f5679d549b0ffe9a34b06f3b8a9f31
Dan Mohl <dmohl@yahoo.com>
2010-04-07 14:50:31 -0500

    Refactored ChordClient commands

--- src/Polyphony/ChordClient.fs
+++ src/Polyphony/ChordClient.fs
@@ -7,23 +7,29 @@ open ChordServerProxy
 let remoteServer = ConfigurationManager.AppSettings.Item("RemoteServer")
 let localServer = ConfigurationManager.AppSettings.Item("LocalServer")
 
+let RunPutCommand server (inputArguments:string[]) (chordServerProxy:IChordServerProxy) = 
+    let valueOption = chordServerProxy.CallServer localServer inputArguments.[0] inputArguments 
+    match valueOption with
+    | Some _ -> sprintf "PUT Key:%A Value:%A" inputArguments.[1] inputArguments.[2] :> obj       
+    | None -> sprintf "The PUT was not successful" :> obj
+
+let RunGetCommand server (inputArguments:string[]) (chordServerProxy:IChordServerProxy) = 
+    let rec getValue server localServerForRecursionStopCheck =
+        let valueOption = chordServerProxy.CallServer server inputArguments.[0] inputArguments 
+        match valueOption with
+        | Some value -> value   
+        | None when localServerForRecursionStopCheck = localServer -> "The Key was not found" :> obj
+        | None -> getValue remoteServer localServer
+    getValue localServer ""
+
 let RunCommand(input:string) (chordServerProxy:IChordServerProxy) : obj =
     let inputArguments = input.Split(' ')
     let result = 
         match inputArguments.[0] with
         | "put" ->
-            let valueOption = chordServerProxy.CallServer localServer inputArguments.[0] inputArguments 
-            match valueOption with
-            | Some _ -> sprintf "PUT Key:%A Value:%A" inputArguments.[1] inputArguments.[2] :> obj       
-            | None -> sprintf "The PUT was not successful" :> obj
+            RunPutCommand localServer inputArguments chordServerProxy
         | "get" -> 
-            let rec getValue server localServerForRecursionStopCheck =
-                let valueOption = chordServerProxy.CallServer server inputArguments.[0] inputArguments 
-                match valueOption with
-                | Some value -> value   
-                | None when localServerForRecursionStopCheck = localServer -> "The Key was not found" :> obj
-                | None -> getValue remoteServer localServer
-            getValue localServer ""
+            RunGetCommand localServer inputArguments chordServerProxy
         | _ -> "Unknown command" :> obj   
     Console.WriteLine(result) |> ignore
     result
