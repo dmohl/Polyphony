ea3c4140e2754f557adad930a17d762b3e1f7b25
Dan Mohl <dmohl@yahoo.com>
2010-04-10 08:23:35 -0500

    Added support for operation timeouts

--- src/Polyphony/ChordServerProxy.fs
+++ src/Polyphony/ChordServerProxy.fs
@@ -1,24 +1,34 @@
 ﻿module ChordServerProxy
 
 open System
+open System.Configuration
 open System.ServiceModel
 open System.Runtime.Serialization
 open ChordCommon
 open ChordServerProxyCommands
 open ChordContracts
+open SettingsProvider
 
 type IChordServerProxy = interface   
     abstract CallServer : server:string -> operationContract:CommandType -> inputArguments:string[] -> obj option
 end
 
+let GetOperationTimeout() =
+    let settingsProvider = new SettingsProvider() :> ISettingsProvider
+    let serviceOperationTimeout = settingsProvider.GetApplicationSetting("ServiceOperationTimeout")
+    let mutable operationTimeout = 5
+    Int32.TryParse(serviceOperationTimeout, ref operationTimeout) |> ignore
+    new TimeSpan(0, 0, operationTimeout)
+
 type ChordServerProxy() = 
     interface IChordServerProxy with
         member this.CallServer server operationContract inputArguments =
-            let service = new ChannelFactory<IChordServer>(
-                                new NetTcpBinding(), server)  
+            let binding = new NetTcpBinding()
+            let service = new ChannelFactory<IChordServer>(binding, server)  
             try                    
                 try
-                    let proxy = service.CreateChannel()   
+                    let proxy = service.CreateChannel()
+                    (proxy :?> IContextChannel).OperationTimeout <- GetOperationTimeout()
                     RunCommand proxy operationContract inputArguments
                 with
                 | ex -> 
